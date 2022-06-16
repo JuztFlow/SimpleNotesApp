86b0b498febcaff2501f5a2ffed105d6fcb0cfe6
Florian Eßwein <f.esswein@t-online.de>
2022-06-16 18:02:10 +0200

    initialize project

---
+++ app/src/main/java/de/hka/esfl1011/oeha1016/simplenotesapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.hka.esfl1011.oeha1016.simplenotesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
